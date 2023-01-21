0d616a8652e746460bd297f01c2ab28ac0018227
Tommy Carver <tommymasoncarver@gmail.com>
2023-01-21 13:07:31 -0800

    sqllite

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,23 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+@app.route('/db')
+def db():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM users').fetchall()
+    conn.close()
+    print(posts[0]['id'])
+    return '<h1>Hello, World!</h1>'
+
+    
---
+++ backend/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    access_key TEXT NOT NULL,
+    secret_key TEXT NOT NULL,
+    username TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS requests;
+
+CREATE TABLE requests (
+    id TEXT PRIMARY KEY NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    current_status TEXT CHECK( current_status IN ('open','active','failed', 'closed', 'disabled', 'cancelled') ) NOT NULL,
+    secret_key TEXT NOT NULL,
+    user TEXT NOT NULL,
+    FOREIGN KEY(user) REFERENCES users(id)
+);
