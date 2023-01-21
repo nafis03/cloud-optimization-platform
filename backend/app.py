12a411add3e72980188b6817cd6fb46e4b9e4f98
Tommy Carver <tommymasoncarver@gmail.com>
2023-01-21 15:42:55 -0800

    for kevy

--- backend/app.py
+++ backend/app.py
@@ -22,35 +22,32 @@ def login():
     aws_access_key_id=data['access_key'],
     aws_secret_access_key=data['secret_key'])
     ec2_resource = session.client('ec2', region_name='us-west-2')
-    # response = ec2_resource.request_spot_instances(
-    #     SpotPrice='0.10',
-    #     DryRun=False,
-    #     # ClientToken must be unique
-    #     ClientToken='strin-0asssoog',
-    #     InstanceCount=1,
-    #     Type='one-time',
-    #     LaunchSpecification={
-    #         'ImageId': 'ami-095413544ce52437d',
-    #         'KeyName': 'awskey',
-    #         'SecurityGroups': ['default'],
-    #         'InstanceType': 't2.micro',
-    #         'EbsOptimized': True,
-    #         'Monitoring': {
-    #             'Enabled': True
-    #         },
-    #         'SecurityGroupIds': [
-    #             'sg-0b549f5ce144da1be',
-    #         ]
-    #     }
-    # )
-    # print(response)
-    res = ec2_resource.describe_spot_instance_requests(
-        DryRun=False,
-        SpotInstanceRequestIds=[
-            'sir-zij6h97j',
-        ],
+    response = ec2_resource.run_instances(
+        MaxCount= 1,
+        MinCount=1,
+        InstanceType='t2.micro',
+        ImageId='ami-095413544ce52437d',
+        KeyName='awskey',
+        Monitoring= {
+            'Enabled':True,
+        },
+        InstanceMarketOptions={
+        'MarketType': 'spot',
+        'SpotOptions': {
+            'MaxPrice': '.05',
+            'SpotInstanceType': 'one-time',
+        }
+    },
     )
-    print(res)
+    spot_request_id = response['Instances'][0]['SpotInstanceRequestId']
+    print(spot_request_id)
+    # res = ec2_resource.describe_spot_instance_requests(
+    #     DryRun=False,
+    #     SpotInstanceRequestIds=[
+    #         'sir-i3fygyxh',
+    #     ],
+    # )
+    # print(res)
     return jsonify(isError= False,
                     message= "Success",
                     statusCode= 200,
