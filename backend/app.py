ac006e916c50d151b209d359c91fc9842bf721a2
KevVo <kevinvo246@hotmail.com>
2023-01-21 17:14:26 -0800

    User database writing

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,15 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def writeUserDB(id: str, secret: str) -> None:
+    conn = get_db_connection()
+    conn.cursor()
+    conn.execute("INSERT INTO users (access_key, secret_key, username) VALUES (?, ?, ?)",
+            (f"{id}", f"{secret}", 'f{id}'))
+    conn.commit()
+    conn.close()
+
+
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
@@ -21,6 +30,7 @@ def login():
     session = boto3.Session(
     aws_access_key_id=data['access_key'],
     aws_secret_access_key=data['secret_key'])
+    writeUserDB(data['access_key'], data['secret_key'])
     ec2_resource = session.client('ec2', region_name='us-west-2')
     response = ec2_resource.run_instances(
         MaxCount= 1,
@@ -41,6 +51,8 @@ def login():
     )
     spot_request_id = response['Instances'][0]['SpotInstanceRequestId']
     print(spot_request_id)
+    print("")
+    print(response)
     # res = ec2_resource.describe_spot_instance_requests(
     #     DryRun=False,
     #     SpotInstanceRequestIds=[
@@ -59,6 +71,7 @@ def db():
     users = conn.execute('SELECT * FROM users').fetchall()
     usersData = {}
     for user in users:
+        print(user['access_key'])
         usersData[user['username']] = user['id']
     conn.close()
     return jsonify( message= "Success",
